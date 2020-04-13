7a5bfc7ec252b436e6fdcc77a2b87ae4c795b86b
jhonnoriega1221 <jhonlex17@gmail.com>
2020-04-13 10:47:46 -0500

    Se corrigio el contenido para que no lo tape la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Pagina2Component } from './pagina2/pagina2.component';
+import { Error404Component } from'./error404/error404.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch:'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'pagina2', component: Pagina2Component }
+  { path: 'pagina2', component: Pagina2Component },
+  { path: '**', redirectTo: 'error404', pathMatch:'full'}
 ];
 
 @NgModule({
