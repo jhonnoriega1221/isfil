93cb353d427a04da96b5b0bcf359fa7bf64cf36b
BuriedSoup <58407784+BuriedSoup@users.noreply.github.com>
2020-06-22 22:24:36 -0500

    Integración con firebase
    La integración está lista pero:
    Debe crearse el servicio de bd
    Deben hacerse los metodos correspondientes
    Debe crearse el modelo activo
    etc...

--- src/app/view/list-activo/list-activo.component.ts
+++ src/app/view/list-activo/list-activo.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+class activo {
+  nombre: string;
+  cantidad: BigInteger;
+  desc: string;
+  lab: string;
+  funcion: string;
+}
 
 @Component({
   selector: 'app-list-activo',
@@ -6,10 +16,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-activo.component.css']
 })
 export class ListActivoComponent implements OnInit {
+  private collectionName = "activos";
+  items: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private db: AngularFirestore) 
+  {
+    this.items = db.collection('activos').valueChanges();
   }
 
+  ngOnInit(): void 
+  {
+    console.log(this.items);
+  }
 }
